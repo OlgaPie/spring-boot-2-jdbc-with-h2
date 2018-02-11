81606c88f68e2ae29300733c961d91c6c76ce70b
Ola Olitta <olgapie@mail.ru>
2018-02-11 16:02:22 +0300

    changed SpringBoot2JdbcWithH2Application

--- src/main/java/springboot/jdbc/h2/example/SpringBoot2JdbcWithH2Application.java
+++ src/main/java/springboot/jdbc/h2/example/SpringBoot2JdbcWithH2Application.java
@@ -10,7 +10,7 @@ import springboot.jdbc.h2.example.student.Student;
 import springboot.jdbc.h2.example.student.StudentJdbcRepository;
 
 @SpringBootApplication
-public class SpringBoot2JdbcWithH2Application /*implements CommandLineRunner*/ {
+public class SpringBoot2JdbcWithH2Application {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
@@ -21,14 +21,4 @@ public class SpringBoot2JdbcWithH2Application /*implements CommandLineRunner*/ {
         SpringApplication.run(SpringBoot2JdbcWithH2Application.class, args);
     }
 
-/*    @Override
-    public void run(String... args) throws Exception {
-
-        logger.info("Student id 10001 -> {}", repository.findById(10001L));
-        logger.info("Student name is Kylie -> {}", repository.findIdByFirstName("Kylie"));
-        logger.info("Inserting -> {}", repository.insert(new Student(10010L, "Justin", "Timberlake")));
-        logger.info("Update 10003 -> {}", repository.update(new Student(10001L, "Name-Updated", "New-Surname")));
-        repository.deleteById(10002L);
-        logger.info("All users -> {}", repository.findAll());
-    }*/
 }
